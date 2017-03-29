52c834594411d2215a701e57141b5242f929de3b
Edmund Wippler <ed.wippler@gmail.com>
2017-03-29 18:55:16 -0500

    fixed error with listener

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var recipeRoute = require('./routes/recipe.routes.js');
 var userRoute = require ('./routes/user.routes.js');
 var mongoConnection = require('./modules/mongo-connection');
 
-var portDecision = process.env.PORT || 5000
+var portDecision = process.env.PORT || 5000;
 
 // Serve back static files
 app.use(express.static(path.join(__dirname, './public')));
@@ -27,8 +27,6 @@ app.use(decoder.token);
 
 app.use('/user', userRoute);
 
-var
-app.set('port', portDecision);
-app.listen(app.get('port'), function() {
-    console.log('Listening on port: ', app.get('port'));
+app.listen(portDecision, function() {
+    console.log('Listening on port: ', portDecision);
 });
