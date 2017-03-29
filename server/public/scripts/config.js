82447ff15b9cc0a80fd2b292a16b6df8c523e15f
Edmund Wippler <ed.wippler@gmail.com>
2017-03-29 18:10:44 -0500

    added styling to navbar and schedule view

---
+++ server/public/scripts/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCsFKk_gHSnFBlzu1HEMulzFrkFS9Ll_oc",
+    authDomain: "wip-it-up.firebaseapp.com",
+    databaseURL: "https://wip-it-up.firebaseio.com",
+    storageBucket: "wip-it-up.appspot.com",
+    messagingSenderId: "778179613292"
+  };
+  firebase.initializeApp(config);
--- server/public/scripts/controllers/nav.controller.js
+++ server/public/scripts/controllers/nav.controller.js
@@ -1,14 +1,19 @@
 myApp.controller('NavController', ['AuthUserFactory', function(AuthUserFactory) {
   var self = this;
   // console.log('nav controller running');
-  self.testMessage = 'Hello World, this is the home controller test message';
+  self.testMessage = 'Hello World, this is the nav controller test message';
   self.isCollapsed = true;
+  self.isOpen = false;
 
-self.toggleNav = function(){
-  self.isCollapsed = !self.isCollapsed;
-}
+  self.toggleNav = function(){
+    self.isCollapsed = !self.isCollapsed;
+  };
+
+  self.toggleDropdown = function(){
+    self.isOpen = !self.isOpen;
+  };
 
   self.logOut = function() {
     AuthUserFactory.logOut();
-  }
+  };
 }]); // end controller code block
